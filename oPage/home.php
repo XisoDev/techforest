f599114170697fb93d324466d765c9e268fd23bc
xiso <xiso@xiso.co.kr>
2019-07-20 15:38:00 +0900

    ajax 예제

--- oPage/home.php
+++ oPage/home.php
@@ -4,13 +4,19 @@
 <a href="<?=getUrl('company')?>" class="btn btn-block btn-primary">기업용</a>
 <a href="<?=getUrl('technician')?>" class="btn btn-block btn-warning">기술자용</a>
 
+
+<?php
+
+/* ajax 예제
 <script type="text/javascript">
-    console.log("A");
     jQuery(document).ready(function($){
         var params = {};
         params["is_view"] = "Y";
 
-        var ret_obj = exec_json("company.index",params);
-        console.log(ret_obj);
+        exec_json("company.index",params,function(ret_obj){
+            console.log(ret_obj);
+        });
+
     });
 </script>
+*/
