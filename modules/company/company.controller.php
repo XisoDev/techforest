1bd556531e2a450941d01ecb37e3ca516cf5d74d
gsjsoop <dahyun712@gmail.com>
2019-07-20 14:54:07 +0900

    현재수정사항까지

---
+++ modules/company/company.controller.php
@@ -0,0 +1,16 @@
+<?
+
+class companyController{
+
+
+  // function hire_ing(){
+  //   global $oDB;
+  //
+  //   $oDB->where("job_end_date","","")
+  //   $oDB->where("c_idx",$logged_info->c_idx);
+  //   $row = $oDB->get("TF_hire_tb");
+  //
+  //   return $row;
+  // }
+
+}
