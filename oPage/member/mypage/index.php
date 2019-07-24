d76d758c4a21b94603817c4403744e001bdabc48
gsjsoop <dahyun712@gmail.com>
2019-07-24 16:51:30 +0900

    내정보수정 페이지

--- oPage/member/mypage/index.php
+++ oPage/member/mypage/index.php
@@ -1,20 +1,31 @@
 
+
 <div class="container">
     <div class="content_padding px-0">
         <a href="#" onclick="history.back();"><i class="xi-arrow-left xi-2x"></i></a>
 
         <a href="/proc.php?act=member.procLogout" class="pull-right btn btn-primary btn-round btn-xs">로그아웃</a>
     </div>
-    <div class="row">
-        <div class="col-5 mx-auto px-auto">
-            <div class="position-relative">
-                <div class="avatar square mb-2" style="background-image:url('/layout/none/assets/images/no_avatar.png');">
-                </div>
-                <i class="xi-camera position-absolute text-white" style="right:0;bottom:0; font-size:28px;"></i>
-                <i class="xi-camera position-absolute text-secondary" style="right:1px;bottom:1px; font-size:26px;"></i>
-            </div>
-        </div>
-    </div>
+
+      <div class="row">
+          <div class="col-5 mx-auto px-auto">
+              <div class="position-relative">
+                  <div class="">
+                    <?if(!$logged_info['m_picture']){?>
+                        <img src="/layout/none/assets/images/no_avatar.png" alt="basic_picture">
+                    <?}else{?>
+                      <img src="../../img/<?=$logged_info['m_picture']?>" class="avatar" id="my_picture" alt="picture">
+                    <?}?>
+                  </div>
+                  <label for="picture_upload" class="position-absolute" style="right:0;bottom:0;">
+                    <i class="xi-camera position-absolute text-white" style="right:0;bottom:0; font-size:28px;"></i>
+                    <i class="xi-camera position-absolute text-secondary" style="right:1px;bottom:1px; font-size:26px;"></i>
+                  </label>
+                  <input type="file" id="picture_upload" style="display:none;">
+              </div>
+          </div>
+      </div>
+
     <div class="content_padding text-center weight_lighter">
         <span class="btn-round btn-xs btn-primary"><?=$logged_info['m_id']?></span>
         <h6 class="weight_lighter mt-3"><i class="xi-mail text-secondary"></i> <?=$logged_info['m_email']?></h6>
@@ -39,3 +50,22 @@
         <span class="badge"><i class="xi-angle-right"></i></span>
     </li>
 </ul>
+
+<script type="text/javascript">
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              $('#my_picture').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
+    $("#picture_upload").change(function(){
+      readURL(this);
+    });
+
+</script>
--- oPage/member/mypage/myprofile.php
+++ oPage/member/mypage/myprofile.php
@@ -1,8 +1,18 @@
+<style media="screen">
+  .border_none{
+    border : none;
+    outline: none;
+    width : 40px;
+    text-align: center;
+  }
+</style>
+
 <div class="container">
     <div class="content_padding px-0">
         <a href="#" onclick="history.back();"><i class="xi-arrow-left xi-2x"></i></a>
         <h5 class="weight_normal">내 정보 설정</h5>
     </div>
+    <?if($logged_info['is_commerce'] == 'Y'){?>
     <form class="tf_underline_form">
         <div class="form-group">
             <label for=""><i class="xi-building"></i> 기업명</label>
@@ -24,4 +34,41 @@
             <a href="#" onclick="jQuery(this).prev().removeAttr('readonly');" class="bg-red btn btn-xs btn-round">변경</a>
         </div>
     </form>
+    <?}else{?>
+      <form class="tf_underline_form">
+          <div class="form-group">
+              <label for=""><i class="xi-building"></i> 이름</label>
+              <input type="text" class="form-control" id="" value="<?=$logged_info['m_name']?>" readonly="readonly">
+          </div>
+          <div class="form-group">
+              <label for=""><i class="xi-call"></i> 휴대폰번호</label>
+                <select id="change_phone" style="display:none;">
+                  <?
+                    $phonenumber = explode("-", $logged_info['m_phone']);
+                    $phone_arr = array("전화번호 선택", "02", "031", "032", "033", "041", "042", "043", "044", "051", "052", "053", "054", "055", "061", "062", "063", "064", "010", "070");
+
+                    for($i = 0; $i < count($phone_arr); $i++) {
+                      if($phone_arr[$i] == $logged_info[0]) {
+                        echo "<option value=\"" . $phone_arr[$i] . "\" selected=\"selected\">" . $phone_arr[$i] . "</option>";
+                      } else {
+                        echo "<option value=\"" . $phone_arr[$i] . "\">" . $phone_arr[$i] . "</option>";
+                      }
+                    }
+                  ?>
+                </select>
+                <div class="form-control" style="border:none;">
+                  <input type="text" class="border_none" id="m_phonenumber1" maxlength="3" value="<?=$phonenumber[0]?>"  readonly="readonly">-
+                  <input type="text" class="border_none" id="m_phonenumber2" maxlength="4" value="<?=$phonenumber[1]?>"  readonly="readonly">-
+                  <input type="text" class="border_none" id="m_phonenumber3" maxlength="4" value="<?=$phonenumber[2]?>"  readonly="readonly">
+                </div>
+              <button type="button" id="change_phone_btn" onclick="" class="bg-red btn btn-xs btn-round">변경</button>
+              <button type="button" id="change_phone_btn_ok" onclick="" class="bg-red btn btn-xs btn-round" style="display:none;">수정완료</button>
+          </div>
+          <div class="form-group">
+              <label for=""><i class="xi-mail"></i> 이메일</label>
+              <input type="email" class="form-control" id="" placeholder="이메일 주소를 입력하세요" value="<?=$logged_info['m_email']?>" readonly="readonly">
+              <a href="#" onclick="jQuery(this).prev().removeAttr('readonly');" class="bg-red btn btn-xs btn-round">변경</a>
+          </div>
+      </form>
+    <?}?>
 </div>
