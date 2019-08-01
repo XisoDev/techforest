a3d886f7a4711c0bfad1906287898d5a2c2ef054
xiso <xiso@xiso.co.kr>
2019-08-02 07:39:42 +0900

    지원자 상세보기 완료

--- oPage/company/application.view.php
+++ oPage/company/application.view.php
@@ -5,73 +5,202 @@
 
 <div class="container" style="position:relative; z-index:1;">
     <div class="content_padding px-0 pb-1">
-        <a href="#" class="pull-right btn btn-primary btn-xxs btn-round">더보기 +</a>
         <h6>기본정보</h6>
-    </div>
-    <div class="tech-card-slick">
-        <?php for($i=1; $i<=4; $i++){?>
-            <div class="tech_card bg-white overflow-hidden">
-                <div class="row">
-                    <div class="col-5 pt-4 pb-0 my-0 pl-4">
-                        <div class="avatar square" style="background-image:url('/layout/none/assets/images/no_avatar.png');">
-                        </div>
-                    </div>
-                    <div class="col-7 pl-0 ml-0">
-                        <div class="star_rating pull-right p-2 xxs_content">
-                            <span class="badge-warning btn-round xxs_content">적합도</span>
-                            <i class="text-warning xi-star"></i>
-                            <i class="text-warning xi-star"></i>
-                            <i class="text-warning xi-star-o"></i>
-                        </div>
-                        <div class="clearfix"></div>
-                        <h6 class="weight_normal my-1 text-left">나상호 (56세)</h6>
-                        <p class="m-0 text-left weight_lighter xs_content">한 곳에서 묵묵히 오래...</p>
-                        <p class="text-left xs_content p-0 m-0"><span class="bg-red icon_wrap"><i class="xi-wrench"></i></span> 경력 : 기계/제조/연구직</p>
-                    </div>
+        <div class="row">
+            <div class="col-5 pt-4 pb-0 my-0 pl-4 mx-auto">
+                <div class="avatar square" style="background-image:url('/layout/none/assets/images/no_avatar.png');">
                 </div>
-                <p class="xxs_content text-right"><i class="xi-clock-o"></i> 지원일자 2019.05.30 10:40:18</p>
-                <a href="<?=getUrl('company','application',100)?>" class="btn btn-block btn-warning mt-3 rounded-0">지원자 정보 상세보기</a>
             </div>
-        <?php } ?>
+        </div>
     </div>
 
 
-    <div class="content_padding px-0 pb-1">
-        <h6>추천 기술자</h6>
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-warning mt-0 rounded-0 mb-3">책임감있는 열정적인 10년차 용접사!</span>
+        <ul class="xs_content text-left">
+            <li>이름 : 이학남[남자]</li>
+            <li>생년월일 : 1992.06.30</li>
+            <li>주소 : 부산시 사상구 모라로 22</li>
+            <li>희망급여 : 회사내규에 따름</li>
+            <li>희망직종 : 건설/조선</li>
+        </ul>
     </div>
-    <div class="flex-card-slick">
-        <?php for($i=1; $i<=4; $i++){?>
-            <div class="tech_card bg-white overflow-hidden">
-                <div class="avatar square" style="margin: 10px 25px; background-image:url('/layout/none/assets/images/no_avatar.png');">
+
+
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-light mt-0 rounded-0 mb-3">경력간단요약</span>
+        <p>성실한 10년차 용접사</p>
+    </div>
+
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-light mt-0 rounded-0 mb-3">학력</span>
+        <ul class="xs_content text-left my-3">
+            <li>학교 : ABCD 학교</li>
+            <li>졸업연도 : 1999.02</li>
+            <li>전공 : 금형,기계제조</li>
+            <li>학점 : 4.21/4.5</li>
+        </ul>
+    </div>
+
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-light mt-0 rounded-0 mb-3">경력</span>
+        <ul class="xs_content text-left my-3">
+            <li>학교 : ABCD 학교</li>
+            <li>졸업연도 : 1999.02</li>
+            <li>전공 : 금형,기계제조</li>
+            <li>학점 : 4.21/4.5</li>
+        </ul>
+    </div>
+
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-light mt-0 rounded-0 mb-3">자격증</span>
+        <ul class="xs_content text-left my-3">
+            <li>학교 : ABCD 학교</li>
+            <li>졸업연도 : 1999.02</li>
+        </ul>
+        <hr class="mx-4" />
+        <ul class="xs_content text-left my-3">
+            <li>전공 : 금형,기계제조</li>
+            <li>학점 : 4.21/4.5</li>
+        </ul>
+    </div>
+
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-light mt-0 rounded-0 mb-3">어학</span>
+        <ul class="xs_content text-left my-3">
+            <li>학교 : ABCD 학교</li>
+            <li>졸업연도 : 1999.02</li>
+        </ul>
+        <hr class="mx-4" />
+        <ul class="xs_content text-left my-3">
+            <li>전공 : 금형,기계제조</li>
+            <li>학점 : 4.21/4.5</li>
+        </ul>
+    </div>
+
+    <div class="tech_card bg-white overflow-hidden pt-0 my-3">
+        <span class="btn btn-block btn-light mt-0 rounded-0 mb-3">관련서류보기</span>
+        <ul class="xs_content text-left my-3">
+            <li>
+<!--                다운로드 링크-->
+                <a href="#">
+                <span class="pull-right mr-3"><i class="xi-download" style="font-size:16px;"></i></span>
+                [이력서] ic_laucher_1024.png
+                </a>
+            </li>
+            <li>
+<!--                다운로드 링크-->
+                <a href="#">
+                <span class="pull-right mr-3"><i class="xi-download" style="font-size:16px;"></i></span>
+                [이력서] ic_laucher_1024.png
+                </a>
+            </li>
+            <li>
+<!--                다운로드 링크-->
+                <a href="#">
+                <span class="pull-right mr-3"><i class="xi-download" style="font-size:16px;"></i></span>
+                [이력서] ic_laucher_1024.png
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <a href="#" class="btn btn-block btn-primary">이력서 전체보기</a>
+
+</div>
+
+<button data-toggle="modal" data-target="#interview_suggestion" class="btn btn-block btn-warning btn-lg rounded-0 fixed-bottom">면접 제안하기</button>
+
+
+<div class="modal fade" id="interview_suggestion" tabindex="-1" role="dialog" aria-labelledby="tech_forest_modal_window" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content text-center" style="border-radius:10px">
+            <a href="#" class="text-white pull-right text-right" style="margin-top:-40px;" onclick="jQuery('#interview_suggestion').modal('hide');" ><i class="xi-close xi-2x"></i></a>
+            <div class="square avatar bg-red mx-auto" style="width:120px; margin-top:-60px; background-image:url('/oPage/ncenter/images/header_icon.png');"></div>
+            <div class="content_padding">
+                <h5 class="weight_lighter"><span class="red">면접 요청방법</span>을<br />선택 해 주세요.</h5>
+                <div class="px-3">
+
+                <button onclick="jQuery('#interview_suggestion').modal('hide');jQuery('#interview_sms').modal('show');" class="btn btn-block btn-danger btn-round mt-3">문자메세지 발송</button>
+<!--                    이렇게 쓰시면 미리 내용을 채워서 공고등록자 휴대전화로 바로 문자전송가능-->
+<!--                <a class="btn btn-block btn-danger btn-round mt-3" href="sms:+821057595999&amp;body=%EA%B8%B0%EC%88%A0%EC%9E%90%EC%88%B2%20%EC%A7%80%EC%9B%90%EC%9E%90%EB%8B%98%EA%BB%98%20%EB%A9%B4%EC%A0%91%EC%9A%94%EC%B2%AD%20%EB%93%9C%EB%A6%BD%EB%8B%88%EB%8B%A4.">문자메세지 발송</a>-->
+                <a class="btn btn-block border-danger text-danger btn-round mt-3" href="tel:+821057595999">지원자에게 직접 전화</a>
                 </div>
-                <h6 class="weight_normal mb-3">나상호 (56세)</h6>
-                <p class="text-left xxs_content"><span class="bg-red icon_wrap"><i class="xi-wrench"></i></span> 주요경력 : 기계 | 연구직</p>
-                <p class="text-left xxs_content"><span class="bg-red icon_wrap"><i class="xi-map-marker"></i></span> 희망지역 : 부산</p>
-                <a href="<?=getUrl('company','application',100)?>" class="btn btn-block btn-warning mt-3 rounded-0">이력서 보기</a>
             </div>
-        <?php } ?>
-    </div>
-
-
-    <div class="content_padding px-0 pb-1">
-        <h6>면접자 현황</h6>
-    </div>
-    <div class="tech-card-slick slick-white-dots">
-        <?php for($i=1; $i<=4; $i++){?>
-            <div class="tech_card bg-white overflow-hidden">
-                <h6 class="color_primary pt-2 pb-0 my-0 mt-2">Co2용접/tig/배관 가능 하신 분 모집</h6>
-                <hr class="mx-2" />
-                <ul class="text-left ml-3 sm_content">
-                    <li class="weight_lighter xs_content">지원자명 : 이학남</li>
-                    <li class="weight_lighter xs_content">면접요청 : 문자</li>
-                    <li class="weight_lighter xs_content">요청일시 : 19.07.18 19:22</li>
-                    <li class="weight_lighter xs_content">면접일 : 19.08.01 12:00</li>
-                </ul>
-            </div>
-        <?php } ?>
+            <button class="mt-2 btn btn-block btn-light" onclick="jQuery('#interview_suggestion').modal('hide');" style="border-radius:10px;">닫기</button>
+        </div>
     </div>
 </div>
-<div class="bg-primary" style="height:130px; margin-top:-105px; position:relative; z-index:0;"></div>
+
+
+<div class="modal fade" id="interview_sms" tabindex="-1" role="dialog" aria-labelledby="tech_forest_modal_window" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content text-center" style="border-radius:10px;">
+            <a href="#" class="text-white pull-right text-right" style="margin-top:-40px;" onclick="jQuery('#interview_sms').modal('hide');jQuery('#interview_suggestion').modal('show');" ><i class="xi-close xi-2x"></i></a>
+            <div class="square avatar bg-red mx-auto" style="width:120px; margin-top:-60px; background-image:url('/oPage/ncenter/images/header_icon.png');"></div>
+            <div class="content_padding" style="overflow-y: scroll; max-height:450px;">
+                <h4 class="weight_lighter red">면접 제안하기</h4>
+                <form method="post" action="/proc.php?act=" class="text-left">
+
+                    <label class="mt-3">공고명</label>
+                    <input type="text" class="form-control" value="PCL 및 로봇제어 기술자분 모집" />
+
+                    <label class="mt-3">면접일시</label>
+                    <input type="text" class="form-control xiso_date mb-1" />
+                    <div class="row">
+                        <div class="col-6 pr-1">
+                            <select class="form-control"><option value="">오전 08시</option></select>
+                        </div>
+                        <div class="col-6 pl-1">
+                            <select class="form-control"><option value="">30분</option></select>
+                        </div>
+                    </div>
+
+                    <label class="mt-3">면접장소</label>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control" />
+                        <button class="btn btn-danger rounded-0">검색</button>
+                    </div>
+                    <input type="text" class="form-control" value="상세주소 입력" />
+
+
+                    <label class="mt-3">면접시 지참 서류</label>
+                    <div class="input-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="none">
+                            <label class="custom-control-label" for="none">없음</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="none">
+                            <label class="custom-control-label" for="none">신분증</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="none">
+                            <label class="custom-control-label" for="none">이력서</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="none">
+                            <label class="custom-control-label" for="none">기타</label>
+                        </div>
+                    </div>
+                    <input type="text" class="form-control" placeholder="기타서류를 입력 해 주세요"/>
+
+                    <label class="mt-3">발신번호</label>
+                    <input type="text" class="form-control" placeholder="- 없이 숫자만 입력" />
+
+                    <label class="mt-3">발신번호 인증</label>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control" />
+                        <button class="btn btn-danger rounded-0">인증</button>
+                    </div>
+
+                    <button class="btn btn-block btn-danger btn-round mt-3">문자메세지 발송</button>
+                </form>
+            </div>
+            <button class="mt-2 btn btn-block btn-light" onclick="jQuery('#interview_sms').modal('hide');jQuery('#interview_suggestion').modal('show');" style="border-radius:10px;">취소</button>
+        </div>
+    </div>
+</div>
 <?php
-$footer_false = true;
+//$footer_false = true;
 ?>
