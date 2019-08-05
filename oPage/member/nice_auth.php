f92cc6941faf3e36eb8fec3a82d4f3c688bf0b1f
gsjsoop <dahyun712@gmail.com>
2019-08-05 19:00:19 +0900

    휴대폰인증

--- oPage/member/nice_auth.php
+++ oPage/member/nice_auth.php
@@ -33,14 +33,15 @@
 
         <div class="row">
             <div class="col mx-0 px-0">
-            <a href="/proc.php?act=member.procMemberNiceAuth" class="btn btn-block btn-primary btn-block btn-round">인증번호 요청</a>
+            <button class="btn btn-block btn-primary btn-block btn-round" onclick="auth()">인증번호 요청</button>
+            <!-- <a href="/proc.php?act=member.procMemberNiceAuth" class="btn btn-block btn-primary btn-block btn-round">인증번호 요청</a> -->
             </div>
         </div>
     </div>
 </div>
 
 <script type="text/javascript">
-function
+function auth(){
   var IMP = window.IMP; // 생략해도 괜찮습니다.
   IMP.init("imp24082824"); // 발급받은 "가맹점 식별코드"를 사용합니다.
 
@@ -49,14 +50,16 @@ function
     merchant_uid: "ORD20180131-0000011"
   }, function (rsp) { // callback
     if (rsp.success) {
-      ...,
+
       // 인증 성공 시 로직,
-      ...
+
     } else {
-      ...,
+
       // 인증 실패 시 로직,
-      ...
+
     }
   });
+}
+
 
 </script>
