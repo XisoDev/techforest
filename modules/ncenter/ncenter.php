ebf87f825a4a4fe8dfd592cad0d8c1243c96ab37
기술자숲 <gsjsoop@gisuljasup-ui-MacBook-Air.local>
2019-08-28 22:02:17 +0900

    알림 커밋

--- modules/ncenter/ncenter.php
+++ modules/ncenter/ncenter.php
@@ -15,8 +15,31 @@ class ncenterView
 
         $output = new Object();
 
+        $output->add('member_notice',$this->member_notice());
+
         $set_template_file = "ncenter/list.php";
 
         return $output;
+    }
+
+    function member_notice(){
+      global $oDB;
+
+      $m_idx = $_SESSION['LOGGED_INFO'];
+
+      $columns = "mn_idx, mn.m_idx, mn.n_idx, mn.num, n.notice_type, n.division, n.used, ns.agree, mn.reg_date, m_name";
+
+      $oDB->where("mn.m_idx",$m_idx);
+
+      $oDB->join("TF_notice AS n", "n.n_idx = mn.n_idx", "LEFT");
+      $oDB->join("TF_notice_setting AS ns", "ns.n_idx = mn.n_idx", "LEFT");
+      $oDB->join("TF_member_tb AS m", "mn.m_idx = m.m_idx", "LEFT");
+
+      $oDB->orderby("n_idx","ASC");
+
+      $row = $oDB->get("TF_member_notice AS mn",null,$columns);
+
+      return $row;
+
     }
 }
--- oPage/ncenter/list.php
+++ oPage/ncenter/list.php
@@ -5,21 +5,67 @@
         <h5 class="weight_normal">알림센터</h5>
     </div>
     <div class="col-12 mt-2">
-        <?php for($i=1; $i<=5; $i++){ ?>
-        <div class="alert_card">
-            <a href="#" class="btn btn-block btn-danger mt-0 rounded-0"><i class="xi-volume-mute"></i> 알림 도착</a>
-            <div class="content_padding pt-2 pb-2">
-                <div>
-                    <span class="btn btn-danger btn-xxs btn-round">오늘</span>
-                    <span class="pull-right xxs_content"><i class="xi-clock-o"></i> 오전 11:30</span>
-                </div>
-                <div class="mt-2">
-                <p class="my-0 py-0 xs_content weight_bold">구민지 님</p>
-                <p class="my-0 py-0 xs_content"><span class="red">사진이력서 등록</span>이 완료되었습니다.</p>
-                </div>
-                <a href="#" class="mt-2 btn btn-block btn-light">자세히 보기</a>
-            </div>
-        </div>
+        <?php foreach($output->get("member_notice") as $val){
+
+          $reg_time = $val['reg_date'];
+
+	        $endDate = strtotime($reg_time);
+
+	        $today = strtotime(date("Y-m-d H:i:s")); //현재시간
+
+	        $diff = $today - $endDate ;
+
+          $day = floor($diff/(3600*24));
+
+          //알림 발생 시간
+          $reg_H = date("H", strtotime($reg_time));
+          $reg_i = date("i", strtotime($reg_time));
+
+          if($reg_H > 12){
+            $reg_H -= 12;
+            $reg_hour = "오후";
+          }else{
+            $reg_hour = "오전";
+          }
+            $reg_hour .= $reg_H."시".$reg_i."분";
+
+        ?>
+          <div class="alert_card">
+              <a href="#" class="btn btn-block btn-danger mt-0 rounded-0"><i class="xi-volume-mute"></i> 알림 도착</a>
+              <div class="content_padding pt-2 pb-2">
+                  <div>
+                      <span class="btn btn-danger btn-xxs btn-round">
+                        <?
+                        if($day > 0){
+                          echo $day."일전";
+                        }else{
+                          echo "오늘";
+                        }
+                        ?>
+                      </span>
+                      <span class="pull-right xxs_content"><i class="xi-clock-o"></i><?=$reg_hour?></span>
+                  </div>
+                  <div class="mt-2">
+                  <p class="my-0 py-0 xs_content weight_bold"><?=$val['m_name']?> 님</p>
+                  <p class="my-0 py-0 xs_content"><span class="red">
+
+                  <?if($val['n_idx']==6){ ?><span style="color:black"><?=$val['m_name']?>님을 위한</span> 맞춤
+                  <?}else if($val['n_idx']==4){?>[<?}?>
+                    <?=$val['notice_type']?>
+                    <?if($val['n_idx']==5){?>
+                    </span>이 완료되었습니다.
+                    <?}else if($val['n_idx']==6){?>
+                    !</span>
+                    <?}else if($val['n_idx']==1){?>
+                    가 발생</span>했습니다. 확인해보세요!
+                    <?}else if($val['n_idx']==4){?>
+                      ]</span>해당 공고가 종료되었습니다.
+                    <?}?>
+                  </p>
+                  </div>
+                  <a href="#" class="mt-2 btn btn-block btn-light">자세히 보기</a>
+              </div>
+          </div>
         <?php } ?>
     </div>
 </div>
