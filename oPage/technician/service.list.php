fc288eadb57e5d5408396be3b985130889d997ff
xiso <xiso@xiso.co.kr>
2019-09-02 03:07:07 +0900

    PC버전 일부 추가 및 최대 가로크기 960 제한

--- oPage/technician/service.list.php
+++ oPage/technician/service.list.php
@@ -6,36 +6,45 @@
 </section>
 <div class="container pt-lg-5 pb-5 bg-white">
     <div class="mx-lg-auto col-lg-10 py-lg-5 pt-lg-3">
-        <h4 class="d-none d-lg-block mb-4"><i class="xi-lightbulb-o text-warning"></i> 유료서비스 소개</h4>
-    <a class="service_card mb-5 bg-white shadow" href="<?=getUrl('technician','service',10)?>">
-        <div class="row">
-            <div class="col-12 col-lg-7">
-                <div class="thumbnail" style="height:100%; background-image:url('/oPage/technician/images/tech_service.jpg"></div>
-            </div>
-            <span class="btn btn-primary btn-xs d-lg-none">개인</span>
-            <div class="col-12 col-lg-5 py-lg-4 text-center text-lg-left">
-                <div class="px-3 pb-2 pt-4">
-                    <span class="btn btn-primary btn-lg d-lg-inline-block d-none position-static py-2 px-4 mb-3">개인</span>
-                    <h4 class="weight_bold">프리미엄 회원</h4>
-                    <h6 class="weight_lighter">프리미엄 회원권을 이용해보세요.</h6>
+        <h4 class="d-none d-lg-block mb-4"><img src="/oPage/images/imgicons/bulb_yellow.png" height="30" class="imgicon" /> 유료서비스 소개</h4>
+
+
+        <a class="service_card mb-5 bg-white shadow" href="<?=getUrl('company','service',10)?>">
+            <div class="row">
+                <div class="col-12 col-lg-8">
+                    <div class="thumbnail" style="height:100%; background-image:url('/oPage/company/images/company_service2.jpg"></div>
+                </div>
+                <span class="btn btn-primary btn-xs d-lg-none">개인</span>
+                <div class="col-12 col-lg-4 py-lg-4 text-center text-lg-left">
+                    <div class="px-3 pb-2 pt-4 px-lg-0">
+                        <span class="btn btn-danger btn-lg d-lg-inline-block d-none position-static btn-xs py-2 px-4 mb-3">기업</span>
+                        <h4 class="weight_bold">프리미엄 회원</h4>
+                        <h6 class="weight_lighter">프리미엄 회원권을 이용해보세요.</h6>
+                    </div>
+                    <div class="d-none d-lg-block red">
+                        서비스 자세히보기
+                    </div>
                 </div>
             </div>
-        </div>
-    <a class="service_card mb-5 bg-white shadow" href="<?=getUrl('technician','service',10)?>">
-        <div class="row">
-            <div class="col-12 col-lg-7">
-                <div class="thumbnail" style="height:100%; background-image:url('/oPage/technician/images/tech_service.jpg"></div>
-            </div>
-            <span class="btn btn-primary btn-xs d-lg-none">개인</span>
-            <div class="col-12 col-lg-5 py-lg-4 text-center text-lg-left">
-                <div class="px-3 pb-2 pt-4">
-                    <span class="btn btn-primary btn-lg d-lg-inline-block d-none position-static py-2 px-4 mb-3">개인</span>
-                    <h4 class="weight_bold">프리미엄 회원</h4>
-                    <h6 class="weight_lighter">프리미엄 회원권을 이용해보세요.</h6>
+        </a>
+        <a class="service_card mb-5 bg-white shadow" href="<?=getUrl('company','service',10)?>">
+            <div class="row">
+                <div class="col-12 col-lg-8">
+                    <div class="thumbnail" style="height:100%; background-image:url('/oPage/company/images/company_service2.jpg"></div>
+                </div>
+                <span class="btn btn-primary btn-xs d-lg-none">개인</span>
+                <div class="col-12 col-lg-4 py-lg-4 text-center text-lg-left">
+                    <div class="px-3 pb-2 pt-4 px-lg-0">
+                        <span class="btn btn-danger btn-lg d-lg-inline-block d-none position-static btn-xs py-2 px-4 mb-3">기업</span>
+                        <h4 class="weight_bold">프리미엄 회원</h4>
+                        <h6 class="weight_lighter">프리미엄 회원권을 이용해보세요.</h6>
+                    </div>
+                    <div class="d-none d-lg-block red">
+                        서비스 자세히보기
+                    </div>
                 </div>
             </div>
-        </div>
-    </a>
+        </a>
     </div>
 </div>
 
